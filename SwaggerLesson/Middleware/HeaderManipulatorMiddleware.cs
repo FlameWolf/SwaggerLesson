ab5adfb51d80a355a9ea2b39bd3957368ba3c0d5
Radhakrishnan <359136@cognizant.com>
2021-03-23 16:39:28 +0530

    Add project files.

---
+++ SwaggerLesson/Middleware/HeaderManipulatorMiddleware.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SwaggerLesson.Middleware
+{
+	public class HeaderManipulatorMiddleware
+	{
+		private readonly RequestDelegate _next;
+		private readonly ImmutableList<string> _headersToRemove;
+
+		public HeaderManipulatorMiddleware(RequestDelegate next, ImmutableList<string> headersToRemove)
+		{
+			_next = next;
+			_headersToRemove = headersToRemove;
+		}
+
+		public async Task Invoke(HttpContext httpContext)
+		{
+			httpContext.Response.OnStarting(() =>
+			{
+				_headersToRemove.ForEach(headerToRemove =>
+				{
+					if (httpContext.Response.Headers.ContainsKey(headerToRemove))
+					{
+						httpContext.Response.Headers.Remove(headerToRemove);
+					}
+				});
+				return Task.FromResult(0);
+			});
+			await _next(httpContext);
+		}
+	}
+
+	public static class HeaderManipulatorMiddlewareExtensions
+	{
+		public static IApplicationBuilder UseHeaderManipulatorMiddleware(this IApplicationBuilder builder, params string[] headersToRemove)
+		{
+			return builder.UseMiddleware<HeaderManipulatorMiddleware>(headersToRemove.ToImmutableList());
+		}
+	}
+}
